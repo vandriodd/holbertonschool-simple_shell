cae8a981dfbefd0b40dae77312ba1e7a5a0d6504
vandriodd <ludasaadi@gmail.com>
2023-04-18 07:03:20 -0700

    [Organizing] file 1-read_line renamed to prompt

---
+++ prompt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints "$ " & wait for the user to enter a command
+ *
+ * Return: 0 on success
+ */
+int main()
+{
+	size_t size = 1024;
+	char *buffer = malloc(size);
+
+	while (buffer)
+	{
+		printf("$ ");
+		getline(&buffer, &size, stdin);
+		printf("%s", buffer);
+	}
+
+	return (0);
+}
