2f0921de1fb522f74b80e630a62b30c2ee0e44c4
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-04-24 05:21:12 -0700

    [Init] super simple shell

---
+++ super_simple_shell/shell.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+*/
+int main(void)
+{
+}
---
+++ super_simple_shell/tok_l.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 1024
+/**
+ */
+char *to_av(*str)
+{
+    size_t size = SIZE, count = 0;
+    char *buff = *token, **arr = NULL;
+
+    while (printf("$ ") && getline(&buff, &size, stdin) != -1)
+    {
+        arr = malloc(sizeof(char *) * size);
+        token = strtok(buff, " ");
+        while (token)
+        {
+        }
+    }
+}
